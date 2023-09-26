76c6321c72e6206b985a3592b98381913164fbdb
Lee Long <longj@depaulcatholic.org>
2023-09-26 14:13:59 -0400

    some refactoriing of the data access service

--- src/main/java/com/example/demo/dao/PersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -5,18 +5,15 @@ import org.springframework.stereotype.Repository;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fileDao")
-public class PersonDataAccessService implements PersonDao {
-    private final static String FILE_REPOSITORY = "data/people.list";
-    private final static List<Person> DB = new ArrayList<>();
+public class PersonDataAccessService extends FakePersonDataAccessService {
+    private final static String FILE_REPOSITORY = "people.list";
 
     static {
         try (FileInputStream fileIn = new FileInputStream(FILE_REPOSITORY);
-             ObjectInputStream in = new ObjectInputStream(fileIn);) {
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
             DB.addAll((ArrayList) in.readObject());
         } catch (FileNotFoundException ignore) {
         } catch (IOException e) {
@@ -28,7 +25,7 @@ public class PersonDataAccessService implements PersonDao {
 
     private void writeToRepository() {
         try (FileOutputStream fileOut = new FileOutputStream(FILE_REPOSITORY);
-             ObjectOutputStream out = new ObjectOutputStream(fileOut);) {
+             ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
             out.writeObject(DB);
             out.flush();
         } catch (FileNotFoundException ignore) {
@@ -39,46 +36,28 @@ public class PersonDataAccessService implements PersonDao {
 
     @Override
     public int insertPerson(UUID id, Person person) {
-        DB.add(new Person(id, person.getName()));
-        writeToRepository();
-        return 1;
-    }
-
-    @Override
-    public List<Person> selectAllPeople() {
-        return DB;
-    }
-
-    @Override
-    public Optional<Person> selectPersonById(UUID id) {
-        return DB.stream()
-                .filter(person -> person.getId().equals(id))
-                .findFirst();
+        int success = super.insertPerson(id, person);
+        if (success != 0) {
+            writeToRepository();
+        }
+        return success;
     }
 
     @Override
     public int deletePersonById(UUID id) {
-        Optional<Person> personMaybe = selectPersonById(id);
-        if (personMaybe.isEmpty()) {
-            return 0;
+        int success = super.deletePersonById(id);
+        if (success != 0) {
+            writeToRepository();
         }
-        DB.remove(personMaybe.get());
-        writeToRepository();
-        return 1;
+        return success;
     }
 
     @Override
     public int updatePersonById(UUID id, Person update) {
-        return selectPersonById(id)
-                .map(person -> {
-                    int indexOfPersonToUpdate = DB.indexOf(person);
-                    if (indexOfPersonToUpdate >= 0) {
-                        DB.set(indexOfPersonToUpdate, new Person(id, update.getName()));
-                        writeToRepository();
-                        return 1;
-                    }
-                    return 0;
-                })
-                .orElse(0);
+        int success = super.updatePersonById(id, update);
+        if (success != 0) {
+            writeToRepository();
+        }
+        return success;
     }
 }
